67ab09b34ad631d9fa47897c9e904c94574c8692
kmixc <oliverkmiec24@gmail.com>
2024-06-14 00:29:47 -0400

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,12 @@ import '../css/About.css'
 import Logo from '../img/logo.svg'
 import { Link } from 'react-router-dom'
 
+//IMAGES & VIDEO
+import IMG_1 from '../img/Kmixc-1.JPG'
+import IMG_2 from '../img/Kmixc-2.jpg'
+
+//COMPONENTS
+import Footer from '../components/Footer'
 
 export default function About() {
     return (
@@ -11,25 +17,41 @@ export default function About() {
                 <Link to="/"><img className='logo' src={Logo} alt="kmixcvisuals" /></Link>
             </div>
 
-            <div className='title'>
-                <p>ABOUT KMIXC VISUALS</p>
+            <div className='about-section-1'>
+                <p className='text'>Driven by passion, bound by
+                    expertise; our team, the heartbeat
+                    of innovation, sculpting digital
+                    dreams into reality.
+                </p>
             </div>
 
-            <div className='about-me'>
+            <div className='about-picture'>
+                <img src={IMG_1} className='picture' alt="" />
             </div>
 
-            <div className='picture-box'>
-                <div className='picture'>
-                    <img src="https://placehold.co/400x600" alt="" />
-                </div>
-                <div className='info'>
-                    <p>
-                        Hey there! I'm Oliver Kmiec, the brains behind Kmixc Visuals. I wear many hats—videographer, social media manager, and web developer.
-                        With a knack for storytelling, I capture moments that leave a lasting impression. As a social media manager, I craft engaging content strategies to elevate brands. And as a web developer, I design seamless digital experiences that reflect the essence of each brand.
-                        I'm passionate about creativity, innovation, and delivering top-notch solutions to my clients. Let's create something amazing together!
-                    </p>
+            <div className='about-section-2'>
+                <p className='text'>
+                    We’re a studio with diverse roots
+                    that want to help companies and
+                    organizations with everything creative,
+                    art and technology. We work with a
+                    passion for the craft and embrace
+                    innovation in our process.
+                </p>
+            </div>
+
+            <div className='about-section-3'>
+                <div className='big-text'>
+                    <p>OUR</p>
+                    <p>MISSION</p>
                 </div>
             </div>
+
+            <div className='about-section-4'>
+                <img src={IMG_2} className='picture' alt="" />
+            </div>
+
+            <Footer></Footer>
         </div>
     )
 }
--- src/pages/Our_Work.jsx
+++ src/pages/Our_Work.jsx
@@ -1,65 +1,144 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
 import '../css/ourWork.css'
 import Logo from '../img/logo.svg'
 import { Link } from 'react-router-dom'
+import { gsap } from 'gsap';
+import CountUp from 'react-countup';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 import { InstagramEmbed } from 'react-social-media-embed';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import "swiper/swiper-bundle.css";
-import "swiper/css";
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+//VIDEO
+
+// import required modules
+import { Autoplay, Pagination } from 'swiper/modules';
 
 export default function Our_Work() {
+    const progressCircle = useRef(null);
+    const progressContent = useRef(null);
+    const onAutoplayTimeLeft = (s, time, progress) => {
+        progressCircle.current.style.setProperty('--progress', 1 - progress);
+        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    };
+
+    const countEnd = () => {
+        const tl = gsap.timeline();
+        const overlay = document.querySelector('.overlay');
+        tl.to(overlay, { duration: 1, ease: "power4.inOut", y: -4100 });
+
+        const text_tl = gsap.timeline();
+        const text_overlay = document.querySelector('.overlay-transparent');
+        text_tl.to(text_overlay, { duration: 1, ease: "power4.inOut", y: -4100 });
+
+        const logoTl = gsap.timeline();
+        const logo = document.querySelector('.logo');
+        logoTl.set([logo], { autoAlpha: 0, y: 50 });
+        logoTl.to(logo, { autoAlpha: 1, duration: 1, delay: 0.2, ease: "power4.inOut", y: 0 });
+    };
+
     return (
         <div className='our-work-page'>
-            <div className='top'>
-                <div className='contact-top-bar' id='#top'>
-                    <Link to="/"><img className='logo' src={Logo} alt="kmixcvisuals" /></Link>
-                </div>
-                <div>
-                    <a href="#photography">photography</a>
-                    <a href="#videography">videography</a>
-                </div>
 
-                <div>
-                    <h1>check out our work</h1>
-                    <p></p>
+            <div className="overlay-transparent">
+                <CountUp onEnd={countEnd} className='countup-overlay' suffix='%' end={100} />
+            </div>
+            <div className="overlay"></div>
+            <div className='top-bar' id='#top'>
+                <Link to="/"><img className='logo' src={Logo} alt="kmixcvisuals" /></Link>
+            </div>
+            <div className='top'>
+                <div className='our-work-title'>
+                    <h1 className='date'>2023</h1>
+                    <h1 className='title'>DEMO REEL</h1>
                 </div>
 
                 <div className='video-background'>
-                    <iframe src="https://www.youtube.com/embed/kPjIu5laeTg?si=qpe3gYyt__agqVh-?loop=1&controls=0&showinfo=0&rel=0&autoplay=1&mute=1" frameBorder='0' allowFullScreen></iframe>
+                    <video autoplay="autoplay" loop="loop" muted defaultMuted playsInline style={{ maxWidth: "1000%" }} id='myVideo'>
+                        <source src=""></source>
+                    </video>
                 </div>
             </div>
 
 
             <div className='youtube-content'>
                 <h1>HIGHLIGHTS</h1>
-
-                <Swiper className="mySwiper">
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
-                    <SwiperSlide>Slide 4</SwiperSlide>
-                    <SwiperSlide>Slide 5</SwiperSlide>
-                    <SwiperSlide>Slide 6</SwiperSlide>
-                    <SwiperSlide>Slide 7</SwiperSlide>
-                    <SwiperSlide>Slide 8</SwiperSlide>
-                    <SwiperSlide>Slide 9</SwiperSlide>
-                </Swiper>
-
-                <div className='section row'>
-                    <iframe width="900" height="540" src="https://www.youtube.com/embed/9zDUE2rVqrk?si=axII55wRMRNLiXzW&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
-                    <div className='text'>
-                        <h1>Boxing Cinematic</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
-                    </div>
-                </div>
                 <div className='section-rev'>
                     <iframe width="900" height="540" src="https://www.youtube.com/embed/ABxbSUmVDAg?si=DAO-HtJSNDFhY92R&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
                     <div className='text'>
                         <h1>LZ World Tour 2023</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
+                    </div>
+                </div>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 8000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Autoplay, Pagination]}
+                    onAutoplayTimeLeft={onAutoplayTimeLeft}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <div className='section row'>
+                            <iframe width="900" height="540" src="https://www.youtube.com/embed/e75xO0AavQQ?si=OEMAv27ECtJSyWGb&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
+                            <div className='text'>
+                                <h1>Stairs 4u</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='section row'>
+                            <iframe width="900" height="540" src="https://www.youtube.com/embed/sPkOKKX9NQA?si=TUhnFYrBpSg0haEU&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
+                            <div className='text'>
+                                <h1>Stairs 4u</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='section row'>
+                            <iframe width="900" height="540" src="https://www.youtube.com/embed/RA_snXyzdzE?si=ASMU970senIJhNKT&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
+                            <div className='text'>
+                                <h1>Stairs 4u</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='section row'>
+                            <iframe width="900" height="540" src="https://www.youtube.com/embed/Do0sUfW5SU0?si=L7c1PUjeRH4uh2-2&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
+                            <div className='text'>
+                                <h1>Stairs 4u</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <div className="autoplay-progress" slot="container-end">
+                        <svg viewBox="0 0 48 48" ref={progressCircle}>
+                            <circle cx="24" cy="24" r="20"></circle>
+                        </svg>
+                        <span ref={progressContent}></span>
+                    </div>
+                </Swiper>
+
+                <div className='section-rev'>
+                    <iframe width="900" height="540" src="https://www.youtube.com/embed/9zDUE2rVqrk?si=axII55wRMRNLiXzW&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
+                    <div className='text'>
+                        <h1>Boxing Cinematic</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
                     </div>
                 </div>
@@ -71,21 +150,7 @@ export default function Our_Work() {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
                     </div>
                 </div>
-                <div className='section-rev'>
-                    <iframe width="900" height="540" src="https://www.youtube.com/embed/e75xO0AavQQ?si=OEMAv27ECtJSyWGb&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
-                    <div className='text'>
-                        <h1>Stairs 4u</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
-                    </div>
-                </div>
 
-                <div className='section row'>
-                    <iframe width="900" height="540" src="https://www.youtube.com/embed/sPkOKKX9NQA?si=TUhnFYrBpSg0haEU&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
-                    <div className='text'>
-                        <h1>Stairs 4u</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
-                    </div>
-                </div>
                 <div className='section-rev'>
                     <iframe width="900" height="540" src="https://www.youtube.com/embed/eGEUnLA9v6A?si=AykSzxE6R-QAxBke&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
                     <div className='text'>
@@ -94,47 +159,58 @@ export default function Our_Work() {
                     </div>
                 </div>
 
-                <div className='section row'>
-                    <iframe width="900" height="540" src="https://www.youtube.com/embed/sPkOKKX9NQA?si=TUhnFYrBpSg0haEU&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
-                    <div className='text'>
-                        <h1>Millworx</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
-                    </div>
-                </div>
-                <div className='section-rev'>
-                    <iframe width="900" height="540" src="https://www.youtube.com/embed/eGEUnLA9v6A?si=AykSzxE6R-QAxBke&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
-                    <div className='text'>
-                        <h1>Millworx</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
-                    </div>
-                </div>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <div className='section row'>
+                            <iframe width="900" height="540" src="https://www.youtube.com/embed/sPkOKKX9NQA?si=TUhnFYrBpSg0haEU&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
+                            <div className='text'>
+                                <h1>Millworx</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='section-rev'>
+                            <iframe width="900" height="540" src="https://www.youtube.com/embed/eGEUnLA9v6A?si=AykSzxE6R-QAxBke&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
+                            <div className='text'>
+                                <h1>Millworx</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
 
-                <div className='section row'>
-                    <iframe width="900" height="540" src="https://www.youtube.com/embed/Do0sUfW5SU0?si=L7c1PUjeRH4uh2-2&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
-                    <div className='text'>
-                        <h1>Stairs 4u</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
-                    </div>
-                </div>
-                <div className='section-rev'>
-                    <iframe width="900" height="540" src="https://www.youtube.com/embed/RA_snXyzdzE?si=ASMU970senIJhNKT&showinfo=0&rel=0" frameBorder='0' allowFullScreen></iframe>
-                    <div className='text'>
-                        <h1>Stairs 4u</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a t</p>
-                    </div>
-                </div>
             </div>
 
             <div className='instagram-content'>
 
-                <InstagramEmbed url="https://www.instagram.com/p/C5qwW6MrZuC/" width={328} />
-                <InstagramEmbed url="https://www.instagram.com/p/C5GNZQ3uHWz/" width={328} />
-                <InstagramEmbed url="https://www.instagram.com/p/C5ZUmX8r160/" width={328} />
-                <InstagramEmbed url="https://www.instagram.com/p/C5en2ySryXd/" width={328} />
-                <InstagramEmbed url="https://www.instagram.com/p/C0SjExyA5cH/" width={328} />
-                <InstagramEmbed url="https://www.instagram.com/p/CrdcROoAhaI/" width={328} />
-                <InstagramEmbed url="https://www.instagram.com/p/C4_FPMLOEFJ/" width={328} />
-                <InstagramEmbed url="https://www.instagram.com/p/C3-4ueiu63i/" width={328} />
+                <div className='section'>
+                    <h1>younger generations now turn to social networks to research brands more than search engines.</h1>
+                    <InstagramEmbed className='video' url="https://www.instagram.com/p/C5qwW6MrZuC/" width={328} />
+                    <InstagramEmbed className='video' url="https://www.instagram.com/p/C5GNZQ3uHWz/" width={328} />
+                </div>
+
+                <div className='section'>
+                    <InstagramEmbed className='video' url="https://www.instagram.com/p/C5ZUmX8r160/" width={328} />
+                    <InstagramEmbed className='video' url="https://www.instagram.com/p/C5en2ySryXd/" width={328} />
+                    <h1>brands that partner wisely with creators are connecting with new audiences, earning their trust, and gaining cultural capital.</h1>
+                </div>
+
+                <div className='section'>
+                    <InstagramEmbed className='video' url="https://www.instagram.com/p/C0SjExyA5cH/" width={328} />
+                    <InstagramEmbed className='video' url="https://www.instagram.com/p/CrdcROoAhaI/" width={328} />
+                    <InstagramEmbed className='video' url="https://www.instagram.com/p/C4_FPMLOEFJ/" width={328} />
+                </div>
+
+                <InstagramEmbed className='video' url="https://www.instagram.com/p/C3-4ueiu63i/" width={328} />
 
             </div>
         </div>
